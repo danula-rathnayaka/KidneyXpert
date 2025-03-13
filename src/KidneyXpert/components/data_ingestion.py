f93f6b9e5605af5da7030b7d1cbce63b085a0083
Danula <danusathmin@gmail.com>
2025-03-13 13:08:16 +0530

    Update Paths

--- src/KidneyXpert/components/data_ingestion.py
+++ src/KidneyXpert/components/data_ingestion.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import gdown
 from KidneyXpert import logger
 from KidneyXpert.entity.config_entity import DataIngestionConfig
-from KidneyXpert import relative_path
-
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
@@ -16,12 +14,12 @@ class DataIngestion:
         try:
             dataset_url = self.config.source_URL
             zip_download_dir = self.config.local_data_file
-            os.makedirs(relative_path + "artifacts/data_ingestion", exist_ok=True)
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
             logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
 
             file_id = dataset_url.split("/")[-2]
             prefix = 'https://drive.google.com/uc?/export=download&id='
-            gdown.download(prefix + file_id, str(Path(relative_path) / zip_download_dir))
+            gdown.download(prefix + file_id, str(zip_download_dir))
 
             logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
 
@@ -29,7 +27,7 @@ class DataIngestion:
             raise e
 
     def extract_zip_file(self):
-        unzip_path = Path(relative_path) / self.config.unzip_dir
+        unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
-        with zipfile.ZipFile(Path(relative_path) / self.config.local_data_file, 'r') as zip_ref:
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
