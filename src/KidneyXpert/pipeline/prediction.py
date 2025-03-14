2c5faf4427089c044debf5f67bbbc64111d969bf
Danula <danusathmin@gmail.com>
2025-03-14 19:11:54 +0530

    Create github CICD

--- src/KidneyXpert/pipeline/prediction.py
+++ src/KidneyXpert/pipeline/prediction.py
@@ -9,17 +9,22 @@ class PredictionPipeline:
         self.filename = filename
 
     def predict(self):
-        model = load_model(os.path.join("model", "model.h5"))
+        try:
+            model = load_model(os.path.join("artifacts/training", "model.h5"))
 
-        test_image = image.load_img(self.filename, target_size=(224, 224))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis=0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
+            test_image = image.load_img(self.filename, target_size=(224, 224))
+            test_image = image.img_to_array(test_image)
+            test_image = np.expand_dims(test_image, axis=0)
+            result = np.argmax(model.predict(test_image), axis=1)
+            print(result)
 
-        if result[0] == 1:
-            prediction = 'Tumor'
-        else:
-            prediction = 'Normal'
+            if result[0] == 1:
+                prediction = 'Tumor'
+            else:
+                prediction = 'Normal'
 
-        return [{"image": prediction}]
+            return [{"result": prediction}]
+        except OSError as e:
+            return [{"result": "Model not found. Please train the model first."}]
+        except Exception as e:
+            raise e
